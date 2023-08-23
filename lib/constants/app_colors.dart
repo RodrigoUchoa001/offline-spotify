ade4b567d57d202f0c6d7a08d9b3fc7520b8289e
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-08-23 00:05:46 -0300

    adicionada cor de fundo do botao de playlist da tela iniciaal

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static final temaPrincipal = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: const Color(0xFF131213),
+    cardColor: const Color(0xFF2a2b2b),
     iconTheme: const IconThemeData(
       color: Colors.white,
       size: 32,
