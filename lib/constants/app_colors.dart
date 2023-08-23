eb76c3e89cbf3e027ecaf3197bc599ddbf139620
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-08-22 23:15:59 -0300

    adicionado config de icones

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,5 +4,9 @@ class AppColors {
   static final temaPrincipal = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: const Color(0xFF131213),
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+      size: 32,
+    ),
   );
 }
