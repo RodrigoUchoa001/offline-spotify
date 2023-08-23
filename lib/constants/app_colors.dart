8ca839548c5bb15087e3728bb0145b7e44c861e2
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-08-22 22:58:02 -0300

    iniciado classe das cores do app

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final temaPrincipal = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: const Color(0xFF131213),
+  );
+}
