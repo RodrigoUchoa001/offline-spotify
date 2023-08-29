f4d64ea1252205064b1ed497768f76969e677e91
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-08-29 01:11:10 -0300

    agora aplicativo pode rodar com alto refresh rate

--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:offline_spotify/widgets/app_bar.dart';
 import 'package:offline_spotify/widgets/bottom_nav_bar.dart';
 import 'package:offline_spotify/widgets/initial_all_playlists.dart';
 import 'package:offline_spotify/widgets/recommended_playlists.dart';
 
-class TelaInicial extends StatelessWidget {
+class TelaInicial extends StatefulWidget {
   const TelaInicial({super.key});
+
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterDisplayMode.setHighRefreshRate();
+  }
 
   @override
   Widget build(BuildContext context) {
